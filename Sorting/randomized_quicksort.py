f81b462740a541d27b32d85aa620c195575d918d
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2022-01-18 16:16:08 -0600

    added randomized quicksort

---
+++ Sorting/randomized_quicksort.py
@@ -0,0 +1,15 @@
+# Use random sampling to choose the pivot in order to almost always achieve a time complexity of quicksort close to the best case - O (n log n)
+
+from partition import partition
+import random
+
+def randomized_partition(A,p,r):
+    i = random.randint(p,r)
+    A[i], A[r] = A[r], A[i]
+    return partition(A,p,r)
+
+def randomized_quicksort(A,p,r):
+    if p < r:
+        q = randomized_partition(A,p,r) # get the index of the boundary which splits array into two left and right sorted arrays
+        quicksort(A,p,q-1) # elements to the left of the partition
+        quicksort(A,q+1,r) # elements to the right of the partition
