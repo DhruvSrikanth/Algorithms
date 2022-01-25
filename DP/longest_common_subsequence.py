42e297b96e66dff2d62dae5c1b8e4b4ff3017f9c
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2022-01-25 16:43:39 -0600

    added lcs

---
+++ DP/longest_common_subsequence.py
@@ -0,0 +1,27 @@
+# Time complexity is normally O(2^n) but with dp becomes O(n)
+
+def lcs(X , Y):
+    m = len(X)
+    n = len(Y)
+ 
+    L = [[None]*(n+1) for i in range(m+1)]
+ 
+    """Following steps build L[m+1][n+1] in bottom up fashion
+    Note: L[i][j] contains length of LCS of X[0..i-1]
+    and Y[0..j-1]"""
+    for i in range(m+1):
+        for j in range(n+1):
+            if i == 0 or j == 0 :
+                L[i][j] = 0
+            elif X[i-1] == Y[j-1]:
+                L[i][j] = L[i-1][j-1]+1
+            else:
+                L[i][j] = max(L[i-1][j] , L[i][j-1])
+ 
+    # L[m][n] contains the length of LCS of X[0..n-1] & Y[0..m-1]
+    return L[m][n]
+ 
+ 
+X = "AGGTAB"
+Y = "GXTXAYB"
+print ("Length of LCS is ", lcs(X, Y) )
