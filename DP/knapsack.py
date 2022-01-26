07349f07543cd74ac8c16c65e627482d7efc8d20
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2022-01-25 18:35:02 -0600

    made knapsack more efficient

--- DP/knapsack.py
+++ DP/knapsack.py
@@ -12,9 +12,7 @@ def knapsack(W, wt, val, n):
     # Build table K[][] in bottom up manner
     for i in range(1, n + 1):
         for w in range(1, W + 1):
-            if i == 0 or w == 0:
-                K[i][w] = 0
-            elif wt[i-1] <= w:
+            if wt[i-1] <= w:
                 K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w])
             else:
                 K[i][w] = K[i-1][w]
