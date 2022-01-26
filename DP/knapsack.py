86f0eb3faab857097c3ea69677c1028a36d90422
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2022-01-25 18:29:41 -0600

    made knapsack more efficient by reudcing number of computations by (n+w+1)

--- DP/knapsack.py
+++ DP/knapsack.py
@@ -10,8 +10,8 @@ def knapsack(W, wt, val, n):
     K = [[0 for x in range(W + 1)] for x in range(n + 1)]
   
     # Build table K[][] in bottom up manner
-    for i in range(n + 1):
-        for w in range(W + 1):
+    for i in range(1, n + 1):
+        for w in range(1, W + 1):
             if i == 0 or w == 0:
                 K[i][w] = 0
             elif wt[i-1] <= w:
