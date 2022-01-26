55ef1c5955b9063f20202cb98f03fb9436d74e02
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2022-01-25 18:42:21 -0600

    made lcs more efficient

--- DP/longest_common_subsequence.py
+++ DP/longest_common_subsequence.py
@@ -15,16 +15,14 @@ def lcs_dp(X , Y):
     m = len(X)
     n = len(Y)
  
-    L = [[None]*(n+1) for i in range(m+1)]
+    L = [[0]*(n+1) for i in range(m+1)]
  
     """Following steps build L[m+1][n+1] in bottom up fashion
     Note: L[i][j] contains length of LCS of X[0..i-1]
     and Y[0..j-1]"""
-    for i in range(m+1):
-        for j in range(n+1):
-            if i == 0 or j == 0 :
-                L[i][j] = 0
-            elif X[i-1] == Y[j-1]:
+    for i in range(1, m+1):
+        for j in range1, (n+1):
+            if X[i-1] == Y[j-1]:
                 L[i][j] = L[i-1][j-1]+1
             else:
                 L[i][j] = max(L[i-1][j] , L[i][j-1])
