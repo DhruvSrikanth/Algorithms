4154bdcc4bfd015abef5b8e018b96f2f6473b596
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2022-01-25 19:39:51 -0600

    corrected lcs

--- DP/longest_common_subsequence.py
+++ DP/longest_common_subsequence.py
@@ -1,4 +1,4 @@
-# Time complexity is normally O(m*n*2^(m+n)) but with dp becomes O(m*n)
+# Time complexity is normally O(2^(m+n)) but with dp becomes O(m*n)
 
 def lcs(X, Y):
     m = len(X)
@@ -17,9 +17,6 @@ def lcs_dp(X , Y):
  
     L = [[0]*(n+1) for i in range(m+1)]
  
-    """Following steps build L[m+1][n+1] in bottom up fashion
-    Note: L[i][j] contains length of LCS of X[0..i-1]
-    and Y[0..j-1]"""
     for i in range(1, m+1):
         for j in range1, (n+1):
             if X[i-1] == Y[j-1]:
@@ -27,7 +24,6 @@ def lcs_dp(X , Y):
             else:
                 L[i][j] = max(L[i-1][j] , L[i][j-1])
  
-    # L[m][n] contains the length of LCS of X[0..n-1] & Y[0..m-1]
     return L[m][n]
  
  
