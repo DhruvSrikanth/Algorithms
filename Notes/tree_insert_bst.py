3b887641a58695a840a67e4929915dac147d5752
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2022-02-01 20:05:25 -0600

    added tree insert bst

---
+++ Notes/tree_insert_bst.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.Leftchild = self.Rightchild = None
+
+
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, root, val):
+    if root is None:
+        return Node(val)
+
+    else:
+        if root.data <= val:
+            root.Rightchild = self.insert(root.Rightchild, val)
+        else:
+            root.Leftchild = self.insert(root.Leftchild, val)
+
+        return root
+
+    if __name__ == '__main__':
+    tree = Tree()
+    for i in range(10):
+        tree.insert(random.randint(0,100))
