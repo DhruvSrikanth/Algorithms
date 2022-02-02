2a0722191740be8372c1a91447bad8ac8a246cb6
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2022-02-01 19:04:09 -0600

    added psuedocode for heap extract min

--- Notes/psuedocode_extract_min.py
+++ Notes/psuedocode_extract_min.py
@@ -1,7 +1,28 @@
 '''
 
 
+Extarct min returns the root value and then deletes this value from the heap.
+
+def heap_extract_min(A):
+    min = A[1]
+    A[1] = A[heapsize(A)]
+    heapsize(A) = heapsize(A) - 1
+    min_heapify(A, 1, heapsize(A))
+    return min
+
+def min_heapify(A, i, heapsize):
+    if Left(i) <= heapsize[A] and A[Left(i)] < A[i]:
+        smallest = Left(i)
+    else:
+        smallest = i
 
 
+    if Right(i) <= heapsize[A] and A[Right(i)] < A[i]:
+        smallest = Right(i)
+    
+
+    if smallest != i
+        exchange A[i] and A[smallest]
+        min_heapify(A, smallest, heapsize)
 
 '''
