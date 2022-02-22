a52067b5d44cddae470099a5f29bea7d073f4506
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2022-02-22 17:40:26 -0600

    added minimum spanning tree notes

---
+++ Notes/mst.py
@@ -0,0 +1,10 @@
+'''
+
+Minimum Spanning Trees:
+    1. Input -> Connected undirected graph - G(V,E)
+             -> Weight function w
+             -> No constraint on weights like in Dijkstra
+    2. Output -> A spanning tree contains all vertices and a subset of the edges
+
+
+'''
