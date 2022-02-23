84b3498ef8adae62a4d0a8f537c2ab001fdd5499
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2022-02-22 19:23:19 -0600

    added notes for prims mst

--- Notes/psuedocode_prims.py
+++ Notes/psuedocode_prims.py
@@ -3,7 +3,7 @@
 Time Complexity - O(V) for initialization
                 - O(V) for inserting all vertices in to the queue Q
                 - O(V*(insert + extract_min) + 2E*decrease_key)), each function using a min heap can be done in log V time
-                - Total time complexity = O(V log V + E log V)
+                - Total time complexity = O(V log V + E log V) = O(E lg V) since G is connected
 
 def Prim(G, w, r):
     # G is in the adjacency list format
